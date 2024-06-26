e39003f449460e2d1756d1723828023e5b1640a5
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2024-06-26 23:41:34 +0900

    Delete FrontEnd directory

--- FrontEnd/Udemy_React/react-study/src/components/TabButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  return (
-    <li>
-      <button class={isSelected ? "active" : undefined} onClick={onSelect}>
-        {children}
-      </button>
-    </li>
-  );
-}
